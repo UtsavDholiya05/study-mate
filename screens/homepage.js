591e9965b1aad57b49906f0a0acaff3c162027ca
kavyashah16 <162728153+kavyashah16@users.noreply.github.com>
2025-03-31 22:53:04 +0530

    major changes in home

--- screens/homepage.js
+++ screens/homepage.js
@@ -1,360 +1,299 @@
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  StatusBar,
-  Image,
-} from "react-native";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { View, Text, Dimensions, TouchableOpacity } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 
+const { height, width } = Dimensions.get("window"); // Get device width
 
-export default function App() {
+const Homepage = () => {
   return (
-    <View style={styles.container}>
-      {/* <StatusBar barStyle="light-content" /> */}
-
-      {/* Header */}
-      <View style={styles.header}>
-        <Text style={styles.headerTitle}>StudySmart</Text>
-        <View style={styles.headerIcons}>
-          <Image
-            // source={require("../assets/account_circle (3).jpg")}
-            // style={styles.icon}
+    <View style={{ flex: 1, backgroundColor: "#000" }}>
+      <StatusBar backgroundColor="#000" />
+      <View
+        style={{
+          backgroundColor: "#000",
+          paddingVertical: width * 0.09,
+          paddingHorizontal: width * 0.05,
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+          borderBottomWidth: 0.5,
+          borderBottomColor: "#fff",
+        }}
+      >
+        <TouchableOpacity onPress={() => console.log("Menu Clicked")}>
+          <Ionicons
+            style={{ transform: [{ translateY: 23 }] }}
+            name="menu"
+            size={width * 0.11}
+            color="#9CA37C"
           />
-          <Text style={styles.username}>hello, nya</Text>
+        </TouchableOpacity>
+
+        <View
+          style={{
+            position: "absolute",
+            left: "50%",
+            transform: [{ translateX: -width * 0.13 }, { translateY: 20 }],
+          }}
+        >
+          <Text
+            style={{
+              color: "#fff",
+              fontFamily: "PlayfairDisplay_400Regular",
+              fontSize: width * 0.08,
+              fontWeight: "500",
+            }}
+          >
+            StudySmart
+          </Text>
         </View>
       </View>
 
-      <View style={styles.content}>
-        {/* Home Title Section */}
-        <View style={styles.homeSection}>
-          <View style={styles.homeTitleContainer}>
-            <Text style={styles.homeTitle}>HOME</Text>
-            <View style={styles.speechBubble}>
-              <Text style={styles.speechText}>info</Text>
-            </View>
+      <View style={{ backgroundColor: "#000" }}>
+        <TouchableOpacity
+          style={{
+            backgroundColor: "#FFFFF1",
+            marginHorizontal: width * 0.02,
+            marginTop: 10,
+            padding: 20,
+            borderRadius: 10,
+            shadowColor: "#000",
+            shadowOpacity: 0.2,
+            shadowRadius: 4,
+            elevation: 5,
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "flex-end",
+            position: "relative",
+          }}
+        >
+          <TouchableOpacity
+            style={{
+              position: "absolute",
+              top: 10,
+              right: 10,
+            }}
+          >
+            <Ionicons
+              name="person-circle"
+              size={width * 0.12}
+              color="#000"
+            />
+          </TouchableOpacity>
+
+          <View style={{ flex: 0, alignSelf: "flex-end" }}>
+            <Text
+              style={{
+                color: "#000",
+                fontSize: width * 0.18,
+                fontWeight: "bold",
+                fontFamily: "PlayfairDisplay_400Regular",
+                transform: [
+                  { translateY: width * 0.07 },
+                  { translateX: -width * 0.02 },
+                ],
+              }}
+            >
+              HOME
+            </Text>
           </View>
-          <Text style={styles.welcomeText}>
-            Your desk is all set{"\n"}Let's get stuff done
-          </Text>
+
+          <View style={{ flex: 1, alignItems: "flex-end" }}>
+            <Text
+              style={{
+                color: "#000",
+                fontSize: width * 0.03,
+                textAlign: "right",
+                transform: [
+                  { translateY: width * 0.02 },
+                  { translateX: width * 0.02 },
+                ],
+              }}
+            >
+              Your desk is all set, let's get stuff done
+            </Text>
+          </View>
+        </TouchableOpacity>
+
+        {/* Cards Section */}
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            height: height * 0.24,
+          }}
+        >
+          {/* Card 1 - Join a meeting */}
+          <TouchableOpacity
+            style={{
+              backgroundColor: "#9CA37C",
+              marginHorizontal: width * 0.02,
+              marginTop: 10,
+              marginBottom: 5,
+              padding: 20,
+              flex: 1,
+              borderRadius: 10,
+              shadowColor: "#000",
+              shadowOpacity: 0.2,
+              shadowRadius: 4,
+              elevation: 5,
+              justifyContent: "space-between",
+            }}
+          >
+            <Ionicons name="videocam" size={width * 0.1} color="#000" />
+            <Text
+              style={{
+                color: "#000",
+                fontSize: width * 0.05,
+                fontWeight: "bold",
+                fontFamily: "Inter_400Regular",
+                paddingBottom: 20,
+              }}
+            >
+              Join a meeting
+            </Text>
+          </TouchableOpacity>
+
+          {/* Card 2 - EduShorts */}
+          <TouchableOpacity
+            style={{
+              backgroundColor: "#FFFFF1",
+              marginHorizontal: width * 0.02,
+              marginTop: 10,
+              marginBottom: 5,
+              padding: 20,
+              flex: 1,
+              borderRadius: 10,
+              shadowColor: "#000",
+              shadowOpacity: 0.2,
+              shadowRadius: 4,
+              elevation: 5,
+              justifyContent: "space-between",
+            }}
+          >
+            <MaterialIcons name="movie" size={width * 0.1} color="black" />
+            <Text
+              style={{
+                color: "#000",
+                fontSize: width * 0.05,
+                fontWeight: "bold",
+                fontFamily: "Inter_400Regular",
+                paddingBottom: 20,
+              }}
+            >
+              EduShorts
+            </Text>
+          </TouchableOpacity>
         </View>
 
-        {/* Menu Grid */}
-        <View style={styles.menuGrid}>
-          <View style={styles.menuRow}>
-            <View style={styles.card1}>
-              <TouchableOpacity style={styles.button1}>
-                <Text style={styles.buttonText}>get started</Text>
-              </TouchableOpacity>
-              <Text style={styles.title}>Join a meeting</Text>
-              <View style={styles.dot} />
-            </View>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            height: height * 0.24,
+          }}
+        >
+          {/* Card 1 - Study Groups */}
+          <TouchableOpacity
+            style={{
+              backgroundColor: "#FFFFF1",
+              marginHorizontal: width * 0.02,
+              marginTop: 10,
+              marginBottom: 5,
+              padding: 20,
+              flex: 1,
+              borderRadius: 10,
+              shadowColor: "#000",
+              shadowOpacity: 0.2,
+              shadowRadius: 4,
+              elevation: 5,
+              justifyContent: "space-between",
+            }}
+          >
+            <Ionicons name="people" size={width * 0.1} color="#000" />
+            <Text
+              style={{
+                color: "#000",
+                fontSize: width * 0.05,
+                fontWeight: "bold",
+                fontFamily: "Inter_400Regular",
+                paddingBottom: 20,
+              }}
+            >
+              Study Groups
+            </Text>
+          </TouchableOpacity>
 
-            <View style={styles.card}>
-              <TouchableOpacity style={styles.button}>
-                <Text style={styles.buttonText}>get started</Text>
-              </TouchableOpacity>
-              <Text style={styles.title}>EduShorts</Text>
-              <View style={styles.dot} />
-            </View>
-          </View>
-
-          <View style={styles.menuRow}>
-            <View style={styles.card}>
-              <TouchableOpacity style={styles.button}>
-                <Text style={styles.buttonText}>get started</Text>
-              </TouchableOpacity>
-              <Text style={styles.title}>Study Groups</Text>
-              <View style={styles.dot} />
-            </View>
-
-            <View style={styles.card}>
-              <TouchableOpacity style={styles.button}>
-                <Text style={styles.buttonText}>get started</Text>
-              </TouchableOpacity>
-              <Text style={styles.title}>Notes</Text>
-              <View style={styles.dot} />
-            </View>
-          </View>
-
-          <View style={styles.menuRow}>
-            <View style={styles.card5}>
-              <TouchableOpacity style={styles.button}>
-                <Text style={styles.buttonText}>get started</Text>
-              </TouchableOpacity>
-              <Text style={styles.title}>Study Material </Text>
-              <View style={styles.dot} />
-            </View>
-          </View>
+          {/* Card 2 - Notes */}
+          <TouchableOpacity
+            style={{
+              backgroundColor: "#FFFFF1",
+              marginHorizontal: width * 0.02,
+              marginTop: 10,
+              marginBottom: 5,
+              padding: 20,
+              flex: 1,
+              borderRadius: 10,
+              shadowColor: "#000",
+              shadowOpacity: 0.2,
+              shadowRadius: 4,
+              elevation: 5,
+              justifyContent: "space-between",
+            }}
+          >
+            <Ionicons name="document-text" size={width * 0.1} color="#000" />
+            <Text
+              style={{
+                color: "#000",
+                fontSize: width * 0.05,
+                fontWeight: "bold",
+                fontFamily: "Inter_400Regular",
+                paddingBottom: 20,
+              }}
+            >
+              Notes
+            </Text>
+          </TouchableOpacity>
         </View>
+
+        {/* Final Card - Study Material */}
+        <TouchableOpacity
+          style={{
+            backgroundColor: "#FFFFF1",
+            marginHorizontal: width * 0.02,
+            marginTop: 10,
+            padding: 20,
+            borderRadius: 10,
+            shadowColor: "#000",
+            shadowOpacity: 0.2,
+            shadowRadius: 4,
+            elevation: 5,
+            height: height * 0.17,
+            justifyContent: "space-between",
+          }}
+        >
+          <FontAwesome5 name="book" size={width * 0.1} color="black" />
+          <View>
+            <Text
+              style={{
+                color: "#000",
+                fontSize: width * 0.05,
+                fontWeight: "bold",
+                fontFamily: "Inter_400Regular",
+                paddingBottom: 1,
+              }}
+            >
+              Study Material
+            </Text>
+          </View>
+        </TouchableOpacity>
       </View>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#FFFFF1",
-  },
-  header: {
-    backgroundColor: "#000",
-    backgroundColor: "#000",
-    paddingVertical: height * 0.02,
-    paddingHorizontal: width * 0.05,
-    alignItems: "center", 
-  },
-  headerTitle: {
-    color: "#fff",
-    fontSize: 20,
-    fontWeight: "500",
-  },
-  headerIcons: {
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  blueSquare: {
-    width: 16,
-    height: 16,
-    backgroundColor: "#3b5fe2",
-    marginRight: 15,
-  },
-  lightningIcon: {
-    width: 16,
-    height: 20,
-    backgroundColor: "transparent",
-    borderLeftWidth: 10,
-    borderRightWidth: 10,
-    borderBottomWidth: 20,
-    borderLeftColor: "transparent",
-    borderRightColor: "transparent",
-    borderBottomColor: "#3b5fe2",
-    transform: [{ rotate: "20deg" }],
-  },
-  content: {
-    flex: 1,
-    // paddingHorizontal: 15,
-    // paddingTop: 10,
-  },
-  homeSection: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "flex-start",
-    // marginBottom: 20,
-    borderWidth: 2,
-    borderColor: "black",
-    borderRadius: 2,
-    height: 160,
-    // width: 309
-  },
-  homeTitleContainer: {
-    flexDirection: "row",
-    alignItems: "flex-start",
-  },
-  homeTitle: {
-    fontSize: 60,
-    fontWeight: "bold",
-    color: "#000",
-    paddingTop: 90,
-  },
-  speechBubble: {
-    backgroundColor: "#fff",
-    borderRadius: 15,
-    paddingHorizontal: 8,
-    marginTop: 100,
-    borderWidth: 1,
-    borderColor: "#000",
-  },
-  speechText: {
-    fontSize: 12,
-    color: "#000",
-  },
-  welcomeText: {
-    fontSize: 14,
-    // textAlign: "right",
-    paddingRight: 5,
-    color: "#555",
-    marginTop: 110,
-    // maxWidth: "50%",
-  },
-  menuGrid: {
-    flex: 1,
-    // width: 153
-  },
-  menuRow: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    height: 190,
-    // borderWidth: 1,
-    // borderColor: "black",
-    // borderRadius: 2,
-  },
-  menuItem: {
-    flex: 1, // Ensures equal width for all items
-    backgroundColor: "#fff",
-    padding: 20,
-    justifyContent: "space-between",
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 1 },
-    shadowOpacity: 0.1,
-    shadowRadius: 2,
-    elevation: 2,
-    borderWidth: 2, // Ensures a uniform border
-    borderColor: "blue",
-    borderRadius: 10, // Optional: rounded corners for a smoother look
-  },
-  menuRow1: {
-    flexDirection: "row",
-    // marginBottom: 15,
-    height: 169,
-    borderWidth: 2,
-    borderColor: "black",
-    borderRadius: 2,
-  },
-  // menuItem: {
-  //   flex: 1,
-  //   backgroundColor: "#fff",
-  //   // borderRadius: 0,
-  //   padding: 20,
-  //   // marginHorizontal: 5,
-  //   justifyContent: "space-between",
-  //   shadowColor: "#000",
-  //   shadowOffset: { width: 0, height: 1 },
-  //   shadowOpacity: 0.1,
-  //   shadowRadius: 2,
-  //   elevation: 2,
-  // },
-  // menuItem1: {
-  //   flex: 1,
-  //   backgroundColor: "#9CA37C",
-  //   // borderRadius: 0,
-  //   padding: 20,
-  //   // marginHorizontal: 5,
-  //   justifyContent: "space-between",
-  //   shadowColor: "#000",
-  //   shadowOffset: { width: 0, height: 1 },
-  //   shadowOpacity: 0.1,
-  //   shadowRadius: 2,
-  //   elevation: 2,
-  // },
-  emptyItem: {
-    backgroundColor: "transparent",
-    shadowOpacity: 0,
-    elevation: 0,
-  },
-  menuItemTitle: {
-    fontSize: 18,
-    fontWeight: "500",
-    color: "#000",
-  },
-  menuItemTitle1: {
-    fontSize: 18,
-    fontWeight: "500",
-    color: "#9CA37C",
-  },
-  getStartedButton: {
-    backgroundColor: "#a3b18a",
-    alignSelf: "flex-start",
-    paddingVertical: 8,
-    paddingHorizontal: 15,
-    // marginBottom: 0,
-    borderRadius: 20,
-  },
-  getStartedText: {
-    color: "#fff",
-    fontSize: 14,
-  },
-  edushortsItem: {
-    borderColor: "black",
-    borderLeftWidth: 3,
-    // borderRadius: 2
-  },
-  notes: {
-    borderColor: "black",
-    borderLeftWidth: 4,
-  },
-  meet: {
-    color: "#9CA37C",
-  },
-  card: {
-    width: 180,
-    height: "auto",
-    backgroundColor: "#FFFFF1",
-    borderRadius: 5,
-    padding: 10,
-    borderWidth: 3,
-    borderColor: "black",
-    position: "relative",
-  },
-  card1: {
-    width: 180,
-    height: "auto",
-    backgroundColor: "#9CA37C",
-    borderRadius: 5,
-    padding: 10,
-    borderWidth: 3,
-    borderColor: "black",
-    position: "relative",
-  },
-  card5: {
-    width: "100%",
-    height: "auto",
-    backgroundColor: "#FFFFF1",
-    borderRadius: 5,
-    padding: 10,
-    borderWidth: 3,
-    borderColor: "black",
-    position: "relative",
-  },
-  button: {
-    backgroundColor: "#EFEFEF",
-    paddingHorizontal: 3,
-    paddingVertical: 1,
-    borderRadius: 10,
-    alignSelf: "flex-start",
-    borderWidth: 1,
-  },
-  button1: {
-    backgroundColor: "#9CA37C",
-    paddingHorizontal: 3,
-    paddingVertical: 1,
-    borderRadius: 10,
-    alignSelf: "flex-start",
-    borderWidth: 1,
-  },
-  buttonText: {
-    fontSize: 10,
-    color: "#000",
-  },
-  buttonText1: {
-    fontSize: 10,
-    color: "#9CA37C",
-  },
-  title: {
-    fontSize: 16,
-    fontWeight: "bold",
-    marginTop: 5,
-  },
-  dot: {
-    width: 8,
-    height: 8,
-    backgroundColor: "#000",
-    borderRadius: 4,
-    position: "absolute",
-    top: 8,
-    right: 8,
-  },
-  icon: {
-    width: 30,
-    height: 30,
-    borderRadius: 2,
-    tintColor: "white",
-    borderWidth: 2,
-    borderColor: "white",
-  },
-  username: {
-    fontSize: 18,
-    fontWeight: "bold",
-    textDecorationLine: "underline",
-  },
-});
+export default Homepage;
